5ba10f3951e21457fc029d60bcd85824d1a3366c
LDwyer095 <liam.innis.dwyer@gmail.com>
2025-02-19 16:17:47 -0500

    Compleated bookbot python code

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from collections import Counter
-
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         countWords(file_contents)
+        countCharacters(file_contents)
 
-def countWords(file_contents):
-    words = sum(Counter(file_contents.split()))
-    print(words)
+def countWords(text):
+    wordlist = text.split()
+    n = 0
+    for word in wordlist:
+        n = n + 1
+    wordSum = n
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{wordSum} words found in the document \n")
+
+def countCharacters(text):
+    charDic = {}
+    charlist = text.lower()
+    charlist = list(charlist)
+    for i in charlist:
+        charDic[i] = 0
+    for i in charlist:
+        charDic[i] += 1
+    for i in charDic:
+        if i.isalpha() == True:
+            print (f"The '{i}' character was found {charDic[i]} times")
     
 
 main()
